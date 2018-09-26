3cd85625c2eb794f5811cab547fd91800177489f
Timotej Zaťko <timi.zatko@gmail.com>
2018-09-26 18:33:14 +0200

    Fix env variable anme

--- scripts/ide/get-home.js
+++ scripts/ide/get-home.js
@@ -2,8 +2,10 @@ const os = require("os");
 const path = require("path");
 
 module.exports = function () {
-    if (process.env["ATOM_HOME"])
-        return process.env["ATOM_HOME"];
+    const envVarName = `${this.key.toUpperCase()}_HOME`;
+
+    if (process.env[envVarName])
+        return process.env[envVarName];
 
     if (this.osRelative[os.type()])
         return path.resolve(os.homedir(), this.osRelative[os.type()]);
